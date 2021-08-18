0d58ce376e1fee4968a1da08e978d74b53b7edf7
TM-Consulting <mounaim.touinssi@gmail.com>
2021-08-18 19:55:27 +0200

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Login from "./containers/Login";
-
+import Category from "./containers/Category";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/category">
+          <Category />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
